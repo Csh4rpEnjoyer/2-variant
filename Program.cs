2efc1da219b7723acc1e72f662604290aa8969bd
Csh4rpEnjoyer <113981795+Csh4rpEnjoyer@users.noreply.github.com>
2022-09-26 15:11:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+double x = double.Parse(Console.ReadLine());
+double y = double.Parse(Console.ReadLine());
+if (x >= -1 && x <= 1)
+{
+    if (y <= 1 && y >= 0)
+    {
+        if (x + y == 1)
+            Console.WriteLine("Входит");
+        else
+            Console.WriteLine("Не входит");
+    }
+    if (y <= 0 && y >= -1)
+    {
+        if (y != Math.Pow(-x, Math.E))
+            Console.WriteLine("Входит");
+        else
+            Console.WriteLine("Не входит");
+
+    }
+}
+else
+    Console.WriteLine("Не входит");
+
